89e96372d57f88a6fd57299f5e0df3d3f4132ae8
jimender2 <35303639+jimender2@users.noreply.github.com>
2020-10-08 00:29:45 -0400

    Add Client and update Server

--- server.py
+++ server.py
@@ -1,14 +1,22 @@
+#!/usr/bin/env python
+
 import socket
 
 
-def Main():
-    HOST = "0.0.0.0"
-    PORT = 8888
+TCP_IP = "0.0.0.0"
+TCP_PORT = 5005
+BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((HOST, PORT))
-    s.listen(5)
-    c, addr = s.accept()
-    msg = "hello"
-    msg = msg.encode("utf-8")
-    s.sendall(msg)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print("Connection address:", addr)
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data:
+        break
+    print("received data:", data)
+    conn.send(data)  # echo
+conn.close()
